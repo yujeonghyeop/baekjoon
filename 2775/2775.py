1cae6ee8f12fca172ddd112b11d67f4916f20914
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-12-11 00:10:00 +0900

    boj_8

--- 2775/2775.py
+++ 2775/2775.py
@@ -1,5 +1,15 @@
 num = int(input())
+answer = []
 for i in range(num):
+    a = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
     floor = int(input())
     k = int(input())
-    
+    for j in range(1, k+1):
+        a[0].append(j)
+    for p in range(1, floor+1):
+        cnt = 0
+        for q in range(len(a[0])):
+            cnt += a[p-1][q]
+            a[p].append(cnt)
+    answer.append(a[floor][k-1])
+    print(answer[i])
