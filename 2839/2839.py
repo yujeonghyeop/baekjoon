a7737d10c6bec210d3c5aaca453249e0ea1a0d32
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-12-17 21:03:44 +0900

    boj_19

--- 2839/2839.py
+++ 2839/2839.py
@@ -1,17 +1,16 @@
-a=int(input())
-answer=[]
-b=a//5+1
-for i in range(1,b):
-    if (a-5*i)%3==0:
+a = int(input())
+answer = []
+b = a//5+1
+for i in range(1, b):
+    if (a-5*i) % 3 == 0:
         answer.append(i+(a-5*i)//3)
         print(answer, i)
-if a%3==0:
+if a % 3 == 0:
     answer.append(a//3)
-if a%5==0:
+if a % 5 == 0:
     answer.append(a//5)
-if len(answer)==0:
+if len(answer) == 0:
     print(-1)
 else:
     print(answer)
     print(min(answer))
-    
