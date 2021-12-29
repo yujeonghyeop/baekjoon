eb788170a8c540f69a181bb415308ec1ad336984
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-12-29 23:38:28 +0900

    boj_34

---
+++ 10808/10808.py
@@ -0,0 +1,7 @@
+a = input()
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+answer = [0]*25
+for i in range(len(a)):
+    answer[alpha.index(a[i])] += 1
+print(*answer)
