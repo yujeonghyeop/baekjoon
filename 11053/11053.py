ec74becb2a173d48088153c9b6c7197113c40642
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-01-11 10:46:49 +0900

    boj_48

---
+++ 11053/11053.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = list(map(int, input().split()))
+dp = [0 for i in range(n)]
+print(dp)
+for i in range(n):
+    for j in range(i):
+        if a[i] > a[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+    print(dp)
+print(max(dp))
