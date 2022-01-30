666656ebd88351f6ba017efa3f4b7a10a9fde01b
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-01-30 16:17:19 +0900

    boj_78

--- 9663/9663.py
+++ 9663/9663.py
@@ -1,1 +1,21 @@
-import sys
+n = int(input())
+row = [0]*n
+ans = 0
+def check(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x]-row[i])==abs(x-i):
+            return False
+
+    return True
+
+def dfs(x):
+    global ans
+    if x==n:
+        ans+=1
+    else:
+        for i in range(n):
+            row[x] = i
+            if check(x):
+                dfs(x+1)
+dfs(0)
+print(ans)
