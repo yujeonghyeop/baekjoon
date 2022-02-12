438deade2030b478c1dc4fea83170b16ea2fbe91
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-02-12 23:28:11 +0900

    boj_91

---
+++ 1764/1764.py
@@ -0,0 +1,21 @@
+import sys
+a,b = list(map(int,sys.stdin.readline().split()))
+cnt = {}
+ranswer = []
+for i in range(a):
+    cnt[input()] = 1
+for j in range(b):
+    e = input()
+    if e in cnt:
+        cnt[e] +=1
+    else:
+        cnt[e] =1
+answer = list(cnt.items())
+print(answer)
+for i in range(len(answer)):
+    if answer[i][1] == 2: 
+        ranswer.append(answer[i][0])
+print(len(ranswer))
+ranswer.sort()
+for i in range(len(ranswer)):
+    print(ranswer[i])
