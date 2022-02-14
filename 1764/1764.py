4c28532d4f9996783a78c96a9d47a43e27ed185c
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-02-14 23:22:35 +0900

    boj_92

--- 1764/1764.py
+++ 1764/1764.py
@@ -1,5 +1,5 @@
 import sys
-a,b = list(map(int,sys.stdin.readline().split()))
+a, b = list(map(int, sys.stdin.readline().split()))
 cnt = {}
 ranswer = []
 for i in range(a):
@@ -7,13 +7,13 @@ for i in range(a):
 for j in range(b):
     e = input()
     if e in cnt:
-        cnt[e] +=1
+        cnt[e] += 1
     else:
-        cnt[e] =1
+        cnt[e] = 1
 answer = list(cnt.items())
 print(answer)
 for i in range(len(answer)):
-    if answer[i][1] == 2: 
+    if answer[i][1] == 2:
         ranswer.append(answer[i][0])
 print(len(ranswer))
 ranswer.sort()
