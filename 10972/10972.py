571ec3d63c66e378766a110f5a3f8aec8a764812
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-02-15 16:40:42 +0900

    boj_94

---
+++ 10972/10972.py
@@ -0,0 +1,13 @@
+import sys
+n = int(sys.stdin.readline())
+read = list(map(int,sys.stdin.readline().split()))
+cnt = 0
+for i in range(n-1,0,-1):
+    if read[i-1] < read[i]:
+        for j in range(n-1, 0, -1):
+            if read[i-1] < read[j]:
+                read[i-1], read[j] = read[j], read[i-1]
+                read = read[:i] + sorted(read[i:])
+                print(*read)
+                exit()
+print(-1)
