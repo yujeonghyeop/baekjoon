9837060a78249ab4497fc5fab7c71cd84a51ae86
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-02-27 18:33:15 +0900

    boj_101

--- 1248/1248.py
+++ 1248/1248.py
@@ -1,3 +1,31 @@
 import sys
 n = int(sys.stdin.readline())
 read = sys.stdin.readline().split()
+arr = [[1] * n for _ in range(n)]
+s = []
+k=0
+def possible(g):
+    v=0
+    for i in range(g,-1,-1):
+        v += s[i]
+        if arr[i][g] == '+' and v <=0:
+            return False
+        if arr[i][g] == '0' and v!=0:
+            return False
+        if arr[i][g] == '-' and v >=0:
+            return False
+    return True
+def dfs(q):
+    if len(s)==n:
+        print(' '.join(map(str,s)))
+        exit()
+    for i in range(-10,11):
+        s.append(i)
+        if possible(q):
+            dfs(q+1)
+        s.pop()
+for i in range(n):
+    for j in range(i,n):
+        arr[i][j] = read[0][k]
+        k+=1
+dfs(0)
