37a1034a346b2fe9c48b4749b337d8aa013a9488
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2023-01-19 17:00:06 +0900

    2644

--- 2644/2644.py
+++ 2644/2644.py
@@ -1,7 +1,26 @@
 import sys
+
+def dfs(i,depth):
+    depth +=1
+    visit[i] = True
+    if i == b:
+        answer.append(depth)
+    for node in chon[i]:
+        if visit[node] != True:
+            dfs(node,depth)
+
 n = int(input())
-a,b = sys.stdin.readline().split()
+a,b = map(int,sys.stdin.readline().split())
+chon = [[] for _ in range(n+1)]
+visit = [False] * (n+1)
+answer = []
 cnt = int(input())
 for i in range(cnt):
-    c,d = sys.stdin.readline().split()
-    print(c,d)
+    c,d = map(int,sys.stdin.readline().split())
+    chon[c].append(d)
+    chon[d].append(c)
+dfs(a,0)
+if len(answer)==0:
+    print(-1)
+else:
+    print(answer[0]-1)
